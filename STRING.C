c421a68815523945edbc0f9f84c0ed0f6399bd35
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-05 16:07:09 +0530

    string.C

---
+++ STRING.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[25];
+int n;
+printf("\nenter the string");
+scanf("%s",&a);
+printf("enter the  number of times");
+scanf("%d",&n);
+for(i=0;i<n;ii++)
+{
+printf("\n",a);
+}
+}
