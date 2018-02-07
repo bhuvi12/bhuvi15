032b5e3b449c1a072510314e50d07aafe6c3f859
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-07 16:08:39 +0530

    fibonacci.C

---
+++ FIBONACCI.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,n3,range,temp;
+printf("enter the range");
+scanf("%d",&range);
+printf("fibonacci series is.....");
+printf("\n",n1);
+printf("\n",n2);
+temp=2;
+while(temp<range)
+{
+n3=n11+n2;
+temp++;
+printf("\n",n3);
+n1=n2;
+n2=n3;
+}
+}
