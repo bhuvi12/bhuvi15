0b5b616d432fa46ccd997394ce0db28b48cd5ae5
bhuvi12 <31438639+bhuvi12@users.noreply.github.com>
2018-02-07 15:46:59 +0530

    swap.C

---
+++ swap.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the two numbers");
+scanf("%d",&a,&b);
+printf("before swapping",a,b);
+swap(a,b)
+{
+a=a+b;
+b=a-b;
+a=a-b;
+printf("after swapping");
+}
+}
